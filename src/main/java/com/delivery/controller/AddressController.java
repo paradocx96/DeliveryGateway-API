56208f2ae60bc4128480e52eaf2f541bdf9545f6
Navinda Lankesh <adilanga@gmail.com>
2022-05-14 21:18:14 +0530

    Swagger API Doc Init

--- src/main/java/com/delivery/controller/AddressController.java
+++ src/main/java/com/delivery/controller/AddressController.java
@@ -3,6 +3,8 @@ package com.delivery.controller;
 import com.delivery.model.Address;
 import com.delivery.service.AddressService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -23,36 +25,42 @@ public class AddressController {
 
     @GetMapping("/get")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation("Get All Address")
     public List<Address> getAllAddress() {
         return service.getAll();
     }
 
     @GetMapping("/get/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation("Get Address by Address ID")
     public Address getById(@PathVariable String id) {
         return service.getById(id);
     }
 
     @DeleteMapping("/delete/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation("Delete Address by Address ID")
     public String deleteById(@PathVariable String id) {
         return service.deleteById(id);
     }
 
     @PutMapping("/update")
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation("Update Address")
     public String updateAddress(@RequestBody Address address) {
         return service.updateAddress(address);
     }
 
     @PostMapping("/calculate")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation("Calculate Delivery Cost for Address")
     public double calculateDeliveryCost(@RequestBody Address address) {
         return service.calDeliveryCost(address);
     }
 
     @PostMapping("/add")
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation("Create Address")
     public Address createAddress(@RequestBody Address address) {
         return service.saveAddress(address);
     }
--- src/main/java/com/delivery/controller/PostManController.java
+++ src/main/java/com/delivery/controller/PostManController.java
@@ -3,6 +3,7 @@ package com.delivery.controller;
 import com.delivery.model.PostMan;
 import com.delivery.service.PostManService;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -23,35 +24,41 @@ public class PostManController {
 
     @GetMapping("/get")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation("Get All Postman Orders")
     public List<PostMan> getAll() {
         return service.getAll();
     }
 
     @GetMapping("/get/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation("Get Postman Order by ID")
     public PostMan getPostmanById(@PathVariable String id) {
         return service.getByID(id);
     }
 
     @DeleteMapping("/delete/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation("Delete Postman Order by ID")
     public String deletePostmanById(@PathVariable String id) {
         return service.deleteById(id);
     }
 
     @PutMapping("/update")
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation("Update Postman Order")
     public String updatePostman(@RequestBody PostMan postMan) {
         return service.updateModel(postMan);
     }
 
     @PostMapping("/add")
+    @ApiOperation("Create Postman Order")
     @ResponseStatus(HttpStatus.CREATED)
     public PostMan createPostMan(@RequestBody PostMan pm) {
         return service.savePostMan(pm);
     }
 
     @GetMapping("/last")
+    @ApiOperation("Get Last Postman Order")
     @ResponseStatus(HttpStatus.OK)
     public List<PostMan> getLastOrder() {
         return service.getLastOrder();
